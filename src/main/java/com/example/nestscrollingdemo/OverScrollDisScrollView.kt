7387839b47242899d8d88c66d6764d1be266def1
feng912084760 <feng912084760@163.com>
2021-03-27 13:14:49 +0800

    一个滑动可以超过边界（松手回弹）的滚动自定义控件

---
+++ src/main/java/com/example/nestscrollingdemo/OverScrollDisScrollView.kt
@@ -0,0 +1,68 @@
+package com.example.nestscrollingdemo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.ScrollView
+import java.lang.reflect.Field
+
+class OverScrollDisScrollView(cont: Context, attrs: AttributeSet?): ScrollView(cont, attrs) {
+    val tag = "OverScrollDisScrollView"
+    private val overScrollDistance = 500
+
+    constructor(cont: Context) : this(cont, null)
+
+    init {
+        val sClass = ScrollView::class.java
+        var field: Field? = null
+        try {
+            field = sClass.getDeclaredField("mOverscrollDistance")
+            field.isAccessible = true
+            field.set(this, overScrollDistance)
+        } catch (e: NoSuchFieldException) {
+            e.printStackTrace()
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+        }
+        overScrollMode = OVER_SCROLL_ALWAYS
+    }
+
+
+
+    override fun onOverScrolled(scrollX: Int, scrollY: Int, clampedX: Boolean, clampedY: Boolean) {
+        super.onOverScrolled(scrollX, scrollY, clampedX, clampedY)
+//        mScrollY = 0
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        super.onTouchEvent(ev)
+        if (ev != null) {
+            when(ev.action) {
+                MotionEvent.ACTION_UP -> {
+                    val yDown = getYDownScrollRange()
+                    if (mScrollY < 0) {
+                        onOverScrolled(0, 0, false, false)
+                    } else if (mScrollY > yDown) {
+                        onOverScrolled(0, yDown, false, false)
+                    }
+                }
+
+            }
+        }
+
+        return true
+    }
+
+    private fun getYDownScrollRange(): Int {
+        var scrollRange = 0
+        if (childCount > 0) {
+            val child = getChildAt(0)
+            scrollRange = Math.max(
+                0,
+                child.height - (height - mPaddingBottom - mPaddingTop)
+            )
+        }
+        return scrollRange
+    }
+}
